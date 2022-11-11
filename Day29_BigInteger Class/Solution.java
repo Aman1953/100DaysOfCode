fe6e10a9b1d9c7c49dc9971c61bb6a98ba12c8ef
Aman Kumar <mintuaman111@gmail.com>
2022-11-11 19:35:18 +0530

    Day 29

---
+++ Day29_BigInteger Class/Solution.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+        BigInteger no = new BigInteger(n);
+        
+        if(no.isProbablePrime(1))
+        System.out.println("prime");
+        else
+        System.out.print("not prime");
+        bufferedReader.close();
+    }
+}
