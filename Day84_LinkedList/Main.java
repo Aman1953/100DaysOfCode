dfcbf7af8e6e5220dc3bb21e6703c743f85d6313
Aman1953 <mintuaman111@gmail.com>
2023-03-16 11:16:56 +0530

    Day 84

---
+++ Day84_LinkedList/Main.java
@@ -0,0 +1,18 @@
+package com.DSA;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        LinkedList myLinkedList = new LinkedList(1);
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+      //  myLinkedList.printList();
+
+        myLinkedList.append(4);
+        myLinkedList.append(6);
+        myLinkedList.printList();
+    }
+}
